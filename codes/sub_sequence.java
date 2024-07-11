e6df14f76f9f6abae0af6bb7f04040ffc5bd1d1d
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-07-11 03:19:52 +0000

    Add Files

---
+++ codes/sub_sequence.java
@@ -0,0 +1,17 @@
+public class sub_sequence {
+    public static void main(String[] args) {
+        genereate_subSequence("abc","");
+    }
+    public static void genereate_subSequence(String given,String ans){
+        if(given.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch=given.charAt(0);
+
+        genereate_subSequence(given.substring(1), ans+ch);
+         genereate_subSequence(given.substring(1), ans);
+    }
+    
+}
